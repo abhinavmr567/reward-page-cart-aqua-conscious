87aea647dd532cfa9a75a7e0eabce6b947de8d69
abhinavmr567 <106931034+abhinavmr567@users.noreply.github.com>
2022-08-25 20:02:39 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { CartContext } from '../CartContext';
+import {
+    Montserrat_100Thin,
+    Montserrat_100Thin_Italic,
+    Montserrat_200ExtraLight,
+    Montserrat_200ExtraLight_Italic,
+    Montserrat_300Light,
+    Montserrat_300Light_Italic,
+    Montserrat_400Regular,
+    Montserrat_400Regular_Italic,
+    Montserrat_500Medium,
+    Montserrat_500Medium_Italic,
+    Montserrat_600SemiBold,
+    Montserrat_600SemiBold_Italic,
+    Montserrat_700Bold,
+    Montserrat_700Bold_Italic,
+    Montserrat_800ExtraBold,
+    Montserrat_800ExtraBold_Italic,
+    Montserrat_900Black,
+    Montserrat_900Black_Italic,
+} from '@expo-google-fonts/montserrat';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const win = Dimensions.get('window');
+
+export function CartIcon({navigation}) {
+  const {getItemsCount} = useContext(CartContext);
+   let [fontsLoad] = useFonts({
+        Montserrat_100Thin,
+        Montserrat_100Thin_Italic,
+        Montserrat_200ExtraLight,
+        Montserrat_200ExtraLight_Italic,
+        Montserrat_300Light,
+        Montserrat_300Light_Italic,
+        Montserrat_400Regular,
+        Montserrat_400Regular_Italic,
+        Montserrat_500Medium,
+        Montserrat_500Medium_Italic,
+        Montserrat_600SemiBold,
+        Montserrat_600SemiBold_Italic,
+        Montserrat_700Bold,
+        Montserrat_700Bold_Italic,
+        Montserrat_800ExtraBold,
+        Montserrat_800ExtraBold_Italic,
+        Montserrat_900Black,
+        Montserrat_900Black_Italic,
+    })
+
+    if (!fontsLoad) {
+        return <AppLoading />;
+    }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} 
+        onPress={() => {
+          navigation.navigate('Cart');
+        }}
+      >Cart ({getItemsCount()})</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 8,
+    backgroundColor: 'royalblue',
+    height: 32,
+    padding: 9,
+    borderRadius: 32 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'Montserrat_600SemiBold'
+  },
+});
